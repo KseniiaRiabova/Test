4f911dc60d01d26f34591c40d46a03b2a4120736
Kseniia Riabova <kseniaistratova@gmail.com>
2024-02-02 13:10:00 +0100

    Update

--- Test/client/src/App.jsx
+++ Test/client/src/App.jsx
@@ -1,17 +1,22 @@
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
-import DisplayData from './pages/DisplayData.jsx'
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import DisplayData from './pages/DisplayData.jsx';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('https://betest-d6i8.onrender.com')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<DisplayData />} />
+          <Route path="/" element={<DisplayData />} />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
     </>
   );
 }
